07a8901fdec3e5d29d6b62c18167aae5e7ab832d
Caesar Feng <caesar@rcafinnovation.ca>
2021-03-18 22:01:11 -0400

    contact section checkin

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faCoffee, faEnvelopeSquare  } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 @Component({
   selector: 'app-footer',
@@ -6,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
+  faEnvelopeSquare = faEnvelopeSquare;
   constructor() { }
 
   ngOnInit(): void {
